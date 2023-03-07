86cd81eb0cd66ddf5ae3ab0c7e3ade854e50e674
Erhan <erhantezer68@gmail.com>
2023-03-07 23:44:50 +0300

    updated

--- src/context/context.js
+++ src/context/context.js
@@ -14,6 +14,7 @@ const url = "https://63a16242a543280f7754aaa3.mockapi.io/products";
 export const AppProvider = ({ children }) => {
     const [products, setProducts] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
 
     const getProducts = async () => {
         setLoading(true)
@@ -21,18 +22,22 @@ export const AppProvider = ({ children }) => {
             const { data } = await axios(url)
             setProducts(data)
             setLoading(false)
+            setError(false)
         } catch (error) {
             console.log(error)
             setLoading(false)
+            setError(true)
         }
     }
+
+    console.log(products)
 
     useContext(() => {
         getProducts()
     },[])
 
     return (
-        <AppContext.Provider value={{ products, loading }}>
+        <AppContext.Provider value={{ products, loading, error }}>
             {children}
         </AppContext.Provider>
     )
--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import { useGlobalContext } from '../context/context'
+import ProductCard from '../components/ProductCard';
+import CardTotal from '../components/CardTotal';
 
 const ProductList = () => {
-    return (
-        <div>ProductList</div>
-    )
+    const { products, loading, error } = useGlobalContext();
+
+
+    if (loading) {
+        return <p className="text-center text-danger w-100">Loading....</p>
+    }
+
+    if (products.length > 0) {
+        return (
+            <>
+                <article id='product-panel' className='col-md-5'>
+                    <ProductCard/>
+                </article>
+                <article className="col-md-5 m-3">
+                    <CardTotal />
+                </article>
+            </>
+        )
+    }
+
+    if (!error) {
+        return <p className="text-center text-danger w-100">No products data...</p>
+    } else {
+        <p className="text-center text-danger w-100">Error...</p>
+    }
+
+
 }
 
 export default ProductList
