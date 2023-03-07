e62d1390b8480ec1b692f5c991a7c105931e6945
Erhan <erhantezer68@gmail.com>
2023-03-07 16:01:36 +0300

    updated

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Navbar from '../components/Navbar'
+import Main from '../pages/Main'
 
 const AppRouter = () => {
     return (
         <BrowserRouter>
             <Navbar/>
             <Routes>
-                <Route/>
+                <Route path='/' element={<Main/>}/>
                 <Route/>
                 <Route/>
                 <Route/>
