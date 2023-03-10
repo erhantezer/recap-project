e73846e67ebcf6e3c858de403ad2f21ff9a8b804
Erhan <erhantezer68@gmail.com>
2023-03-10 23:45:30 +0300

    updated

--- src/pages/UpdateProduct.jsx
+++ src/pages/UpdateProduct.jsx
@@ -4,6 +4,8 @@ import { useLocation } from 'react-router-dom';
 
 const UpdateProduct = () => {
   const { item } = useLocation();
+  const url = "https://63a16242a543280f7754aaa3.mockapi.io/products";
+  
   return (
     <div className='container'>
       <ProductForm text="Update" />
