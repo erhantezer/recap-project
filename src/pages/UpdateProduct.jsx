137fd58b9d21ea6ab7468f9cf2560fe598d6b785
Erhan <erhantezer68@gmail.com>
2023-03-10 23:39:49 +0300

    updated

--- src/pages/UpdateProduct.jsx
+++ src/pages/UpdateProduct.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ProductForm from '../components/ProductForm'
+import { useLocation } from 'react-router-dom';
 
 const UpdateProduct = () => {
+  const { item } = useLocation();
   return (
     <div className='container'>
-      <ProductForm text = "Update"/>
+      <ProductForm text="Update" />
     </div>
   )
 }
