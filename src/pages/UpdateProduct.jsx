56a304122af8cbfdc6f2a166133aa1f66197c80c
Erhan <erhantezer68@gmail.com>
2023-03-10 23:47:50 +0300

    updated

--- src/pages/UpdateProduct.jsx
+++ src/pages/UpdateProduct.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ProductForm from '../components/ProductForm'
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const UpdateProduct = () => {
   const { item } = useLocation();
   const url = "https://63a16242a543280f7754aaa3.mockapi.io/products";
-  
+  const navigate = useNavigate(); //! update işlemi tamamlandığında geldiğim sayfaya geri dönmek için
+
   return (
     <div className='container'>
       <ProductForm text="Update" />
